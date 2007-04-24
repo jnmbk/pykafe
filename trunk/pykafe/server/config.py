3e3549a76948d25f82e4e184c839c7a022cf6a00
jnmbk <jnmbk@0ec6e880-0f2a-0410-9a9c-d93e719367fc>
2007-04-24 16:52:16 +0000

    First server codes

---
+++ trunk/pykafe/server/config.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2007, pyKafe Development Team
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# Please read the COPYING file.
+#
+
+#TODO: Read config from database api
+class PykafeConfiguration:
+    class network:
+        port = 23105
+        clientport = 23106
