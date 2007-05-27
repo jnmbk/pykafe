89fff9ebff7b7df59b4701d3753981a87c7a2640
Uğur Çetin <ugur.jnmbk@gmail.com>
2007-05-27 07:31:45 +0000

    member things are done

--- pykafe/server/database.py
+++ pykafe/server/database.py
@@ -13,15 +13,20 @@
 from pysqlite2 import dbapi2 as sqlite
 
 class Database:
+    #TODO: We should switch to QSqlDatabase
     databaseFile = "/home/jnmbk/.pyKafe/pyKafe.db"
 
     def __init__(self):
-        #TODO: Handle exceptions
         self.con = sqlite.connect(self.databaseFile)
         self.cur = self.con.cursor()
 
     def run(self, query, tuple = ()):
-        #TODO: check for sql injection
         self.cur.execute(query, tuple)
         if not 'select' in query: self.con.commit()
         return self.cur.fetchall()
+
+    def runOnce(self, query, tuple = ()):
+        self.cur.execute(query, tuple)
+        if not 'select' in query: self.con.commit()
+        return self.cur.fetchall()
+        self.con.close()
