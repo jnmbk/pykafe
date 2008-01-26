9ca256ea4df9e85d359a8efef79578f23a16f8fe
Uğur Çetin <ugur.jnmbk@gmail.com>
2008-01-26 19:02:06 +0000

    let's begin doing the server again

--- pykafe/mainwindow_server.py
+++ pykafe/mainwindow_server.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from PyQt4 import QtCore
 from PyQt4 import QtGui
 
-import mainwindow_server_ui
+import mainwindow_server_ui, tcpserver
 
 class MainWindow(QtGui.QMainWindow, mainwindow_server_ui.Ui_MainWindow):
     def __init__(self):
         QtGui.QMainWindow.__init__(self)
         self.setupUi(self)
+
+        self.tcpServer = tcpserver.TcpServer(self)
+
+    @QtCore.pyqtSignature("on_main_startButton_clicked()")
+    def on_main_startButton_clicked(self):
+        print "started"
